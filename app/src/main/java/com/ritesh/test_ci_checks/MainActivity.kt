abc54605598bcf110a87a15243df856d86717bea
Ritesh.Singh <Z003CF1@g5w6n9mx6t.dist.target.com>
2024-05-18 05:42:24 +0530

    test

--- app/src/main/java/com/ritesh/test_ci_checks/MainActivity.kt
+++ app/src/main/java/com/ritesh/test_ci_checks/MainActivity.kt
@@ -16,6 +16,7 @@ import com.ritesh.test_ci_checks.ui.theme.TestGithubCIChecksTheme
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    
     enableEdgeToEdge()
     setContent {
       TestGithubCIChecksTheme {
